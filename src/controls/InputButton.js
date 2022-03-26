0b4dd3746f441dbd8962c69e747d69965a2a3710
hauseto2 <hauseto2@fel.cvut.cz>
2022-03-26 17:03:26 +0100

    Added tooltips to input buttons

--- src/controls/InputButton.js
+++ src/controls/InputButton.js
@@ -16,46 +16,49 @@ export default class InputButton extends React.Component {
 
     render() {
         return (
-          <StyledInputButton onMouseDown={this.handleMouseDown}
-                  className="control-button">
-              {this.props.symbol}
-          </StyledInputButton>
+            <StyledInputButton onMouseDown={this.handleMouseDown}
+                               data-tip
+                               data-for={this.props.tooltipId}
+                               className="control-button">
+                {this.props.symbol}
+            </StyledInputButton>
         );
     }
 }
 
 InputButton.propTypes = {
-  symbol: PropTypes.string.isRequired,
-  handleMouseDown: PropTypes.func.isRequired
+    symbol: PropTypes.string.isRequired,
+    handleMouseDown: PropTypes.func.isRequired,
+    tooltipId: PropTypes.string.isRequired
 };
 
 const StyledInputButton = styled.button`
-    display: inline;
-    list-style-type: none;
-    color: #94bffe;
-    cursor: pointer;
-    -webkit-box-shadow: none!important;
-    box-shadow: none!important;
-    font-size: 18px;
-    font-weight: 400;
-    overflow: hidden!important;
-    width: 50px;
-    height: 50px;
-    border: none;
-    background: var(--input-color);
-    position: relative;
-    background: none;
+  display: inline;
+  list-style-type: none;
+  color: #94bffe;
+  cursor: pointer;
+  -webkit-box-shadow: none !important;
+  box-shadow: none !important;
+  font-size: 18px;
+  font-weight: 400;
+  overflow: hidden !important;
+  width: 50px;
+  height: 50px;
+  border: none;
+  background: var(--input-color);
+  position: relative;
+  background: none;
 
-    &:hover {
-      color: white;
-      /*text-shadow: 2px 4px 3px rgba(0,0,0,0.3);*/
-    }
+  &:hover {
+    color: white;
+    /*text-shadow: 2px 4px 3px rgba(0,0,0,0.3);*/
+  }
 
-    &:active {
-      transform: translateY(1px);
-    }
+  &:active {
+    transform: translateY(1px);
+  }
 
-    &:not(:last-child) {
-      margin-right: 0;
-    }
+  &:not(:last-child) {
+    margin-right: 0;
+  }
 `
--- src/controls/InputButtonsPanel.js
+++ src/controls/InputButtonsPanel.js
@@ -1,22 +1,56 @@
 import React from 'react';
 import InputButton from "./InputButton";
 import styled from 'styled-components';
+import ReactTooltip from "react-tooltip";
 
 class InputButtonsPanel extends React.Component {
     constructor(props) {
         super(props);
 
-        this.inputButtonSymbols = ['π', 'σ', 'ρ', '⋈', '⟕', '⟖', '⟗', '⋉', '⋊', '▷', '◁', '∧', '∨', '¬', '≥', '≤', '∩', '∪', '\\', '÷', '⨯', '→', '*', '⟨', '⟩'];
+        this.inputButtonSymbols = [
+            ['π', 'Pi', 'π_{r}(R)'],
+            ['σ', 'Sigma', 'σ_{r = 4}(R)'],
+            ['ρ', 'Rho', 'ρ_{s / r}(R)'],
+            ['⋈', '', ''],
+            ['⟕', '', ''],
+            ['⟖', '', ''],
+            ['⟗', '', ''],
+            ['⋉', '', ''],
+            ['▷', '', ''],
+            ['∧', '', ''],
+            ['∨', '', ''],
+            ['¬', '', ''],
+            ['≥', '', ''],
+            ['≤', '', ''],
+            ['∩', '', ''],
+            ['∪', '', ''],
+            ['\\', '', ''],
+            ['÷', '', ''],
+            ['⨯', '', ''],
+            ['÷', '', ''],
+            ['→', 'renamingArrow', 'R⟨r → S⟩'],
+            ['*', '', ''],
+            ['⟨', 'leftAngle', 'R⟨r = 4]S'],
+            ['⟩', 'rightAngle', 'R[r = 4⟩S'],
+        ];
     }
 
     render() {
         return (
             <StyledInputButtonsPanelWrapper>
                 <StyledInputButtonsPanel>
-                    {this.inputButtonSymbols.map((symbol, idx) =>
-                        <InputButton symbol={symbol}
-                                     key={idx}
-                                     handleMouseDown={this.props.handleMouseDown}/>)}
+                    {this.inputButtonSymbols.map(([symbol, tipId, content], idx) =>
+                        <>
+                            <InputButton symbol={symbol}
+                                         key={idx}
+                                         tooltipId={`usageTip${tipId}`}
+                                         handleMouseDown={this.props.handleMouseDown}/>
+                            {tipId !== '' ? (
+                                <ReactTooltip id={`usageTip${tipId}`} place="left" effect="solid" key={tipId + '-' + idx}>
+                                    {content}
+                                </ReactTooltip>) : <></>}
+                        </>
+                    )}
                 </StyledInputButtonsPanel>
             </StyledInputButtonsPanelWrapper>
         );
