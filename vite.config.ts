1e81f74481d8c00650f387d30410489209c37426
hauseto2 <hauseto2@fel.cvut.cz>
2024-10-27 12:37:49 +0100

    :wrench: Set port to 3000

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+  },
   define: {
     // By default, Vite doesn't include shims for NodeJS/
     // necessary for segment analytics lib to work
